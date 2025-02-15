530b27e053ee9c609747a8eb7e9c70c4b37049d0
Narendra122812 <narendrakumar122812@gmail.com>
2025-02-15 21:25:31 +0530

    Project Initial Setup -Maven

---
+++ src/test/java/com/automationexercise/testcases/Sample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.automationexercise.testcases;
+
+/**
+ * Sample Test
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Happy Testing");
+	}
+
+}
