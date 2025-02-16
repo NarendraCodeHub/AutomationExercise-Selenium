96f6f28f53a816eec7e1d87a68e46b99df197e10
Narendra122812 <narendrakumar122812@gmail.com>
2025-02-16 12:42:33 +0530

    Added BaseTest and Modify Config file

--- src/test/java/com/automationexercise/pages/BaseTest.java
+++ src/test/java/com/automationexercise/pages/BaseTest.java
@@ -1,5 +1,41 @@
 package com.automationexercise.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.automationexercise.utility.BrowserFactory;
+import com.automationexercise.utility.ConfigDataProvider;
+
 public class BaseTest {
+
+	/**
+	 * Global Variable
+	 */
+	public WebDriver driver;
+	public ConfigDataProvider config = new ConfigDataProvider();
+
+	@BeforeClass
+	public void setup() {
+		String url = config.getStagingUrl();
+
+		// Debugging Logs
+		System.out.println("URL: " + url);
+
+		driver = BrowserFactory.startApplication(driver, url);
+
+		if (driver != null) {
+			System.out.println("Driver initialized successfully.");
+		} else {
+			System.out.println("Driver initialization failed.");
+		}
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (driver != null) {
+			BrowserFactory.quitBrowser(driver);
+		}
+	}
 
 }
